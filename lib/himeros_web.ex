011b5a48f4f2e1c6bf5d9d1c0d09e4877b602694
Himeros Engineering <himeros.engineering@gmail.com>
2014-11-11 05:51:49 +0000

    Adding HimerosWeb.Repo to Supervisor.

--- lib/himeros_web.ex
+++ lib/himeros_web.ex
@@ -9,6 +9,7 @@ defmodule HimerosWeb do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(HimerosWeb.Worker, [arg1, arg2, arg3])
+      worker(HimerosWeb.Repo, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html

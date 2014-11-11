a4ae994448f17a701d51adcf09f46be61ddeabea
Himeros Engineering <himeros.engineering@gmail.com>
2014-11-11 05:43:01 +0000

    Adding Ecto and Postgrex deps.

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule HimerosWeb.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {HimerosWeb, []},
-     applications: [:phoenix, :cowboy, :logger]]
+     applications: [:phoenix, :cowboy, :logger, :postgrex, :ecto]]
   end
 
   # Specifies your project dependencies
@@ -23,6 +23,8 @@ defmodule HimerosWeb.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "0.5.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:postgrex, "~> 0.5"},
+     {:ecto, "~> 0.2.0"}]
   end
 end
